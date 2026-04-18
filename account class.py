18e995ecc47206d9bc8a844f425526e2f372d51a
snehasutar28022008-glitch <snehasutar28022008@gmail.com>
2026-04-18 16:05:44 +0530

    Add files via upload

---
+++ account class.py
@@ -0,0 +1,20 @@
+class Account:
+    def __init__(self,bal,acc):
+        self.balance=bal
+        self.account_no=acc
+        #debit method
+    def debit(self,amount):
+        self.balance=self.balance-amount
+        print("RS",amount,"was debited")
+        print("total balance",self.get_balance())
+            #credit method
+    def credit(self,amount):
+        self.balance=self.balance+amount
+        print("RS",amount,"was credited")
+        print("total balance",self.get_balance())
+    def get_balance(self):
+         return self.balance 
+acc1=Account(10000,12345)
+acc1.debit(1000)
+acc1.credit(300)
+
